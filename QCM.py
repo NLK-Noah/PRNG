fe2809845a8f8c5c6a5119cb69445a67ecc33b96
ShinraaL <140214133+ShinraaL@users.noreply.github.com>
2023-11-12 15:05:28 +0100

    QCM.py
    opti

--- QCM.py
+++ QCM.py
@@ -1,158 +1,138 @@
-import qcm
+from qcm import build_questionnaire
 import random
+
+questions = []  # on initialise la liste de question
+
 
 def import_file():
     """Permet d'importer le QCM
 
-    Cette Fonction importe le QCM desiré 
+    Cette Fonction importe le QCM desiré
 
     Args:
-        input: Filename un nom de fichier 
+        input: Filename un nom de fichier
 
     Returns:
             Le QCM desiré
 
     """
-    
-    filename = input("Choisir votre fichier:")
-    questions = qcm.build_questionnaire(filename)
-    print("REPONDEZ UNIQUEMENT PAR LE NUMERO DE LA QUESTION EST NON LA REPONSE !\nENTRER DEUX FOIS LA MÊME RÉPONSE VOU PÉNALISERA !")
-    
+    while (
+        True
+    ):  # ici  je rajoute une while true si jamais l user se trompe lors qui choisit son fichier il a autant de chance qu il veut tant que son fichier n'a pas été trouvé
+        try:
+            filename = input("Choisir votre fichier:")
+            questions = build_questionnaire(filename)
+            print(
+                "\033[91m"
+                + "REPONDEZ UNIQUEMENT PAR LE NUMERO DE LA QUESTION ET NON LA REPONSE !\nENTRER DEUX FOIS LA MÊME RÉPONSE VOUS PÉNALISERA !\n PS: Vous êtes obligé de répondre autant de fois que le nombre de bonne réponses"
+                + "\033[0m"
+            )
+            return questions
+            break  # Sortir de la boucle si le fichier est trouvé avec succès
+        except FileNotFoundError:
+            print("Le fichier n'a pas été trouvé. Veuillez réessayer.")
 
-def choix_cotation():
+
+questions = import_file()
+
+
+def opti_quest(questions):
     """Permet de choisir un choix de Cotation
 
-    Cette fontion permet de choisir une méthode de cotation parmi les suivantes: 1| Cool, 2| Sévère, 3| Anti-Hasard, 4| Toutes 
+    Cette fontion permet de choisir une méthode de cotation parmi les suivantes: 1| Cool, 2| Sévère, 3| Anti-Hasard, 4| Toutes
 
     Args:
-        Un Entier entre 1 et 4 
+        Un Entier entre 1 et 4
 
     Returns:
         "C", étant la méthode de cotation choisie
 
     """
-    print("Choisissez votre méthode de cotation:\n1| Cool.\n2| Sévère.\n3| Anti-Hasard.\n4| Toutes.") #Affiche les diffférents types de cotations
+
+    print(
+        "Choisissez votre méthode de cotation:\n1| Cool.\n2| Sévère.\n3| Anti-Hasard.\n4| Toutes."
+    )  # Affiche les diffférents types de cotations
     cotation = input("Entrez votre méthode de cotation:")
-    x = cotation.lower() 
-    if x == "cool" or "1":
-        correction_cool()
-    elif x == "severe" or "sévere" or "sevère" or "sévère" or "2":
-        correction_severe()
-    elif x == "antihasard" or "anti-hasard" or "anti hasard" or "anti_hasard" or "3":
-        correction_antihasard()
-    elif x == "toutes" or "4":
-        correction_tout()
-    
-
-# Besoin d'un return pour savoir au niveau du feedback quel cotation on a choisi ? 
-               
-
-def show_qr_shuffle():
-    """Permet d'afficher les questions
-
-    Cette fonction permet d'afficher les questions et les réponses possibles du QCM les réponses sont disposées aléatoirement
-
-    Args:
-        Questions 
-        Réponses
-    Return:
-            Les questions et les réponses possibles
-    
-    """ 
-    list_answers = []
-    count = 0
-    # Boucle Qui Affiche les Questions et Shuffle leurs réponses
+    x = cotation.lower()
+    # Initialisation des listes et des count
+    list_feedback = []
+    list_nbre_qst = []
+    list_true = []
+    liste_true2 = []
+    count_cool = 0
+    count_svr = 0
+    count_noluck = 0
+    count_question = -1
+    # Boucle de longueur égale au nombre de questions
     for i in range(len(questions)):
         random.shuffle(questions[i][1])
         print("-----", "\t", str(i + 1), "|", questions[i][0], "\t", "-----")
         compte = 0
+        count_true = 0
         liste = []
-        # Boucle Qui Affiche les réponses et permet de savoir combien il y à de réponses correctes 
+        count_question += 1
+        # Boucle de longueur égale au nombre de réponses
         for l in range(len(questions[i][1])):
             print("\n", "\t", str(l + 1), "|", questions[i][1][l][0])
+
             if questions[i][1][l][1] is True:
                 compte += 1
-        # Boucle qui permet de répondre autant de fois qu'il y a de réponses correctes
+                list_true.append(count_true)
+                count_true += 1
+            else:
+                count_true += 1
+        liste_true2.append(compte)
         for z in range(compte):
             answer = int(input("Entrez le n° de votre réponse:"))
-            list_answers.append(answer)
-            print(liste)
+
             if answer in liste:
                 print("Vous avez déja entré cette réponse dommage...")
+
             elif questions[i][1][answer - 1][1] is True:
-                count += 1
+                count_cool += 1
+                count_svr += 1
+                count_noluck += 1
+
             else:
-                choix_cotation()
+                count_svr -= 1
+                list_feedback.append(answer)
+                list_nbre_qst.append(count_question)
             liste.append(answer)
 
+    if x == "cool" or x == "1":
+        print("Selon la cotation cool votre notes est de:", count_cool)
 
+    elif x == "severe" or x == "sévere" or x == "sevère" or x == "sévère" or x == "2":
+        print("Selon la cotation sévère votre notes est de:", count_svr)
 
+    elif x == "anti-hasard" or x == "3":
+        print("Selon la correction ANTI-TRICHE votre note est de:", count_noluck)
 
-    
+    elif x == "toutes" or x == "4":
+        print("Selon la cotation COOL votre notes est de:", count_cool)
+        print("Selon la cotation SEVERE votre notes est de:", count_svr)
+        print("Selon la correction ANTI-TRICHE votre note est de:", count_noluck)
 
-def correction():
-    """Permet d'avoir la cote reçue sur le QCM 
-    
-    Cette fonction permet de calculer le nombres de points obtenu par l'utilisateur selon son choix de cotatiion 
+    print("list_true", list_true)
+    print("liste_true2", liste_true2)
 
-    Args:
-        les réponses de l'utilisateur lors du QCM 
-        les réponses correctes du QCM 
-        les méthodes de correction 
+    for t in range(len(list_feedback)):
+        print(
+            "Vous vous êtes trompés aux questions:\n",
+            questions[list_nbre_qst[t]][0],
+            "\t",
+            "Vous avez entré",
+            questions[list_nbre_qst[t]][1][list_feedback[t] - 1][0],
+            "|",
+            questions[list_nbre_qst[t]][1][list_feedback[t] - 1][2],
+        )
 
-    Return:
-        les points obtenus par l'utilisateur lors du QCM selon son choix de cotation 
-        
-    """
-    def correction_cool():
-        count += 0
-    def correction_severe():
-        count -= 1
-    def correction_anti_hasard():
-         count_noluck == 0
-
-    def correction_tout():
-        correction_cool()
-        print("Cotation 'Cool':",count_cool)
-        correction_severe()
-        print("Cotation 'Sévère':",count_severe)
-        correction_anti_hasard()
-        print("Cotation 'Anti_Hasard':",count_noluck)
-
-    feedback()
-
-def feedback():
-    """Permet d'avoir un feedback
-
-    Cette fonction permet d'afficher la cote selon la méthode choisie et des feedback sur les éventuelles erreurs
-
-    Args:
-        récupère les feedback dans le fichier qcm.py
-        les  réponses aux questions et si y en a un qui en à bon et qui a un feedback 
-
-    Returns:
-        return le feedback de la question mauvaise 
-            
-    """
-    print("")
-    print("Session Feedback:")
-    for i in range(len(questions)):
-        print(str(i + 1), questions[i][0])
-    for l in range(len(questions[i][1])):
-        print(questions[i][1][l][2])
-#On sait juste l'afficher
 
 def main():
-    
-# 1) Integrer le QCM.
-    import_file()
-# 2 ) Choix de cotation.
-    choix_cotation()
-# 4 ) Afficher les questions du QCM aléatoires
-    show_qr_shuffle()
-# 5) Correction du QCM.
-    correction()
-# 6 )  Sortie du Feedback avec les cotations.
-    feedback()
-if __name__ == "__main__":
+    # 1) Integrer le QCM qui est direcement faites lors du lancement de choix_cotation(questions)
+    # 2 ) Choix de cotation.
+    opti_quest(questions)
+
+
+if __name__ == "__main__":  # excutation du code
     main()
